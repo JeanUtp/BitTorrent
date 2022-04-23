d0950f6fa1a5a3a74884b6d2badbec242a963628
JeanUtp <78510036+JeanUtp@users.noreply.github.com>
2022-04-23 14:16:42 -0500

    Update c.php

--- c.php
+++ c.php
@@ -3,9 +3,9 @@
   <head>
   <title>BitTorrent</title>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
-
   </head>
   <body>
+   <?php  ?>
   <br>
          <div class="container">
             <br>
